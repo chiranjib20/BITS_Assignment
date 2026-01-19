7586aa7352b3b95b10df830329826feb60e9768f
chiranjib20 <49760674+chiranjib20@users.noreply.github.com>
2026-01-19 10:49:37 +0530

    Update streamlit_app.py

--- Project-folder/streamlit_app.py
+++ Project-folder/streamlit_app.py
@@ -15,6 +15,9 @@ from sklearn.metrics import (
     confusion_matrix
 )
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 st.set_page_config(
     page_title="Online Shoppers Classification",
     layout="wide"
